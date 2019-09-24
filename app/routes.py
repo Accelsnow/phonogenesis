471b9a0abbb335f081a0f8340f1b81d0440a0644
Snow <acceleratorsnow@gmail.com>
2019-09-24 11:57:52 -0400

    Startup flask app

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, password {}, remember_me={}'.format(form.username.data, form.password.data,
+                                                                                form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('index.html', title='Sign In', form=form)
