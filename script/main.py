d8b66a17201caf94279a8c3ef4f62556b5e2bca1
Altair59 <39001328+Altair59@users.noreply.github.com>
2020-04-03 15:19:10 -0400

    db default data injected and tested

--- script/main.py
+++ script/main.py
@@ -5,7 +5,7 @@ from typing import List, Optional, Dict, Tuple
 
 from script.feature_lib import Particle, import_default_features
 from script.generator import Generator
-import random
+from random import randint
 from script.word import Word
 from script.rules import Rule, RuleFamily, import_default_rules
 from script.sound import Sound
@@ -18,10 +18,10 @@ def _print_result(rst: dict):
     if rst is None:
         print("No result.")
         return
-    print("UR:", rst["UR"])
-    print("SR:", rst["SR"])
-    print("gloss: ", rst["Gloss"])
-    print("RULE: ", rst["rule"])
+    print("UR:", rst["UR"], ",")
+    print("SR:", rst["SR"], ",")
+    print("gloss: ", rst["Gloss"], ",")
+    print("ruleTxt: \"", rst["rule"].split(" ===== ")[0] + "\"")
 
 
 def random_select(families: List[RuleFamily], rules_: List[Rule], family_num: int, rule_num: int) -> List[Rule]:
@@ -66,38 +66,38 @@ if __name__ == '__main__':
 
     templates = import_default_templates(features)  # type: List[Template]
 
-    print("full templates: ")
-    ti = 1
-    for template in templates:
-        print(ti, template)
-        ti += 1
+    # print("full templates: ")
+    # ti = 1
+    # for template in templates:
+    #     print(ti, template)
+    #     ti += 1
 
     rule_data = import_default_rules(features)  # type: Tuple[List[RuleFamily], List[Rule]]
     rule_families = rule_data[0]  # type: List[RuleFamily]
     rules = rule_data[1]  # type: List[Rule]
 
-    print("\nfull rule families:")
-    fi = 1
-    for family in rule_families:
-        print(fi, family)
-        fi += 1
-
-    print("\nfull rules: ")
-    ri = 1
-    for rule in rules:
-        print(ri, rule)
-        ri += 1
+    # print("\nfull rule families:")
+    # fi = 1
+    # for family in rule_families:
+    #     print(fi, family)
+    #     fi += 1
+    #
+    # print("\nfull rules: ")
+    # ri = 1
+    # for rule in rules:
+    #     print(ri, rule)
+    #     ri += 1
 
     gloss_data = import_default_gloss()
     gloss_families = gloss_data[0]
     gloss_groups = gloss_data[1]
 
-    print("\nfull gloss families: ", [str(f) + "\n" for f in gloss_families])
-    print("\nfull gloss groups: ", [str(g) + "\n" for g in gloss_groups])
+    # print("\nfull gloss families: ", [str(f) + "\n" for f in gloss_families])
+    # print("\nfull gloss groups: ", [str(g) + "\n" for g in gloss_groups])
+    #
+    # print("\n==================================================\n")
 
-    print("\n==================================================\n")
-
-    manual_rule_select = 87
+    manual_rule_select = randint(0, 80)
 
     use_rule = rules[manual_rule_select]
     amount = 40
@@ -105,35 +105,33 @@ if __name__ == '__main__':
     phonemes = import_default_randomized_phonemes([use_rule.get_a_matcher(None, None, feature_to_sounds),
                                                    use_rule.get_c_matchers(None, feature_to_sounds),
                                                    use_rule.get_d_matchers(None, feature_to_sounds)])
-    psr = ""
+    psr = "phoneme: \""
     for p in phonemes:
         psr += str(p) + " "
-    print(psr)
-
-    while True:
-        # word = input("\nWord to check: ")
-        word = "θəggix"
-        print(rules[manual_rule_select])
-        print(rules[manual_rule_select].classify(Word(word), phonemes, feature_to_type, feature_to_sounds))
-        print(str(rules[manual_rule_select].apply(Word(word), phonemes, feature_to_type, feature_to_sounds)))
-        break
-
-    # print(use_rule.get_rule_type(phonemes, feature_to_type, feature_to_sounds))
-    # use_templates = templates
+    print(psr + "\",")
     #
+    # while True:
+    #     # word = input("\nWord to check: ")
+    #     word = "θəggix"
+    #     print(rules[manual_rule_select])
+    #     print(rules[manual_rule_select].classify(Word(word), phonemes, feature_to_type, feature_to_sounds))
+    #     print(str(rules[manual_rule_select].apply(Word(word), phonemes, feature_to_type, feature_to_sounds)))
+    #     break
+
+    print("ruleType: \"" + str(use_rule.get_rule_type(phonemes, feature_to_type, feature_to_sounds)) + "\",")
+    use_templates = templates
+
     # print("\nUSING RULE: ", use_rule)
     # print("\nGENERATION AMOUNT:", amount, '\n')
-    #
-    # print("=============INTEREST===============")
-    # interest = rules[manual_rule_select].get_interest_phones(phonemes, feature_to_type, feature_to_sounds)[1]
-    # isr = ""
-    # for i in interest:
-    #     isr += str(i) + " "
-    # print(isr)
-    # print("INT END")
-    #
-    # gen = Generator(phonemes, use_templates, use_rule, 5, feature_to_type, feature_to_sounds)
-    #
-    # result = gen.generate(1, amount, True, False, feature_to_type, feature_to_sounds, gloss_groups)
-    #
-    # _print_result(result)
+
+    interest = rules[manual_rule_select].get_interest_phones(phonemes, feature_to_type, feature_to_sounds)[1]
+    isr = "poi: \""
+    for i in interest:
+        isr += str(i) + " "
+    print(isr + "\",")
+
+    gen = Generator(phonemes, use_templates, use_rule, 5, feature_to_type, feature_to_sounds)
+
+    result = gen.generate(1, amount, True, False, feature_to_type, feature_to_sounds, gloss_groups)
+
+    _print_result(result)
